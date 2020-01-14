4fa2fccc5d03ce814973d6b28bfa97037e08539d
Timur Samkharadze <timur.samkharadze@sysco.no>
2020-01-14 15:14:03 +0100

    functions to writing grid and arcs on SVG drawing

--- svg_utils.py
+++ svg_utils.py
@@ -1,4 +1,5 @@
 import svgwrite
+import math
 
 
 def get_default(out_file_name: str) -> svgwrite.Drawing:
@@ -19,7 +20,7 @@ def get_default(out_file_name: str) -> svgwrite.Drawing:
     :param out_file_name: name of file where result will be stored
     :return: drawing object
     """
-    drawing = svgwrite.Drawing(out_file_name)
+    drawing = svgwrite.Drawing(out_file_name, debug=True)
 
     drawing.attribs['overflow'] = 'scroll'
     drawing.defs.add(svgwrite.container.Style())
@@ -32,3 +33,58 @@ def get_default(out_file_name: str) -> svgwrite.Drawing:
 
     drawing.defs.add(default_filter)
     return drawing
+
+
+def polar_to_cartesian(x, y, radius, angle_degrees):
+    """
+    Function to convert polar coordinates to cartesian
+    :param x:
+    :param y:
+    :param radius:
+    :param angle_degrees:
+    :return:
+    """
+    angle_radians = angle_degrees * math.pi / 180.0
+    return x + (radius * math.cos(angle_radians)), y + (radius * math.sin(angle_radians))
+
+
+def describe_arc(x, y, radius, start_angle, end_angle):
+    """
+    function to draw arcs of different radius and angle
+    :param x:
+    :param y:
+    :param radius:
+    :param start_angle:
+    :param end_angle:
+    :return:
+    """
+    start = polar_to_cartesian(x, y, radius, end_angle)
+    end = polar_to_cartesian(x, y, radius, start_angle)
+
+    large_arc_flag = "0" if end_angle - start_angle <= 180 else "1"
+
+    d = " ".join(
+        ["M", str(start[0]), str(start[1]), "A", str(radius), str(radius), "0", large_arc_flag, "0", str(end[0]),
+         str(end[1])])
+    return d
+
+
+def add_grid(obj: svgwrite.Drawing, s=10):
+    pattern_small_grid = obj.pattern(insert=None, size=(s, s), patternUnits='userSpaceOnUse')
+    pattern_small_grid.attribs['id'] = "smallGrid"
+    path_small = obj.path(f'M {s} 0 L 0 0 0 {s}', stroke='gray', fill='none', style=f'stroke-width:{0.5}')
+    pattern_small_grid.add(path_small)
+
+    pattern_large_grid = obj.pattern(insert=None, size=(s * 10, s * 10), patternUnits='userSpaceOnUse')
+    pattern_large_grid.attribs['id'] = "grid"
+    path_large = obj.path(f'M {s * 10} 0 L 0 0 0 {s * 10}', stroke='gray', fill='none', style=f'stroke-width:{1}')
+    pattern_large_grid.add(path_large)
+
+    rect_large = obj.rect(fill="url(#smallGrid)", size=(s * 10, s * 10))
+    pattern_large_grid.add(rect_large)
+
+    obj.defs.add(pattern_small_grid)
+    obj.defs.add(pattern_large_grid)
+
+    rect_grid = obj.rect(size=('100%', '100%'), fill="url(#grid)")
+    obj.add(rect_grid)
