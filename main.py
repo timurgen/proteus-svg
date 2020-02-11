447053f2aab43b31a112c7addbcbb8c256e3090d
Timur Samkharadze <timur.samkharadze@akersolutions.com>
2020-02-11 14:34:31 +0100

    refactoring

--- main.py
+++ main.py
@@ -1,66 +1,76 @@
-import svgwrite
 import logging
 import sys
-import xml.etree.ElementTree as XMLParse
-from model_context import Context
-from svg_utils import get_default, add_grid
-from handlers import resolve_node_handler, extent_handler
-from proteus_utils import get_model_dimensions_from_plant_model, should_process_child
+import os
+import lxml.etree as xml
+
+from flask import Flask, request, abort, Response
+
+from proteus_lib.model_context import Context, Unit
+from proteus_lib.svg_utils import get_default, add_grid
+from proteus_lib.handlers import process_node
+from proteus_lib.proteus_utils import get_model_dimensions_from_plant_model
 
 ROOT_ELEMENT = 'PlantModel'
-DEBUG = False
+
+THREAD_POOL_SIZE = int(os.environ.get('THREAD_POOL_SIZE', 32))
+APP = Flask(__name__)
+
+PARSER = xml.XMLParser(remove_comments=True)
 
 
-def process_node(node: XMLParse.Element, target: svgwrite.base.BaseElement, model_ctx: Context):
-    node_type = node.tag
-    logging.debug(f'processing {node_type} node')
+@APP.route('/', methods=['POST'])
+def process_file():
+    debug = bool(request.args.get('debug'))
+    grid = request.args.get('grid')
 
-    node_handler = resolve_node_handler(node_type)
-    result = node_handler(node, model_ctx)
+    for file in request.files:
+        input_file = xml.parse(request.files[file], PARSER)
+        drawing = get_default(f'{file}.svg')
 
-    if result:
-        target.add(result)
-        if DEBUG:
-            extent_obj = node.find('Extent')
-            extent_rect = extent_handler(extent_obj, model_ctx)
-            target.add(extent_rect)
-    if should_process_child(node.tag):
-        for child in list(node):
-            process_node(child, result if result else target, model_ctx)
+        plant_model = input_file.getroot()
+        if plant_model.tag != ROOT_ELEMENT:
+            abort(400, f'malformed file {file}, {ROOT_ELEMENT} expected at root level')
+
+        (x_min, y_min, x_max, y_max) = get_model_dimensions_from_plant_model(plant_model)
+
+        pl_info = plant_model.find('PlantInformation')
+        m_unit = Unit[pl_info.attrib['Units']]
+
+        model_context = Context(drawing=drawing,
+                                x_min=x_min * m_unit.value, x_max=x_max * m_unit.value,
+                                y_min=y_min * m_unit.value, y_max=y_max * m_unit.value,
+                                debug=debug,
+                                origin=pl_info.attrib['OriginatingSystem'],
+                                units=m_unit)
+
+        drawing.attribs['width'] = model_context.x_max
+        drawing.attribs['height'] = model_context.y_max
+
+        if grid is not None:
+            add_grid(drawing, int(grid) if grid.isnumeric() else 10)
+
+        process_node(plant_model, drawing, model_context)
+        return Response(drawing.tostring(), mimetype='image/svg+xml')
 
 
 if __name__ == '__main__':
-    # input and output files
-    #input_file_name = '080.xml'
-    #input_file_name = '2601-01.xml'
-    input_file_name = 'text_curve.xml'
-    input_file = XMLParse.parse(input_file_name)
+    logging.basicConfig(stream=sys.stdout, level=logging.INFO)
+    port = int(os.environ.get('PORT', '5000'))
+    if logging.getLogger().isEnabledFor(logging.DEBUG):
+        APP.run(debug=True, host='0.0.0.0', port=port)
+    else:
+        import cherrypy
 
-    # set up logging
-    logging.basicConfig(stream=sys.stdout, level=logging.DEBUG)
+        cherrypy.tree.graft(APP, '/')
+        cherrypy.config.update({
+            'environment': 'production',
+            'engine.autoreload_on': True,
+            'log.screen': False,
+            'server.socket_port': port,
+            'server.socket_host': '0.0.0.0',
+            'server.thread_pool': THREAD_POOL_SIZE,
+            'server.max_request_body_size': 0
+        })
 
-    # root element for Proteus files is always "PlantModel"
-    plant_model = input_file.getroot()
-
-    if plant_model.tag != ROOT_ELEMENT:
-        logging.critical(f'malformed file {input_file_name}, {ROOT_ELEMENT} expected at root level')
-        sys.exit(1)
-
-    pl_info = plant_model.find('PlantInformation')
-
-    # get model dimension borders
-    (min_x, min_y, max_x, max_y) = get_model_dimensions_from_plant_model(plant_model)
-
-    drawing = get_default(f'{input_file_name}.svg')
-    model_context = Context(min_x, max_x, min_y, max_y, pl_info.attrib['OriginatingSystem'], pl_info.attrib['Units'])
-
-    drawing.attribs['width'] = model_context.x_max
-    drawing.attribs['height'] = model_context.y_max
-
-    add_grid(drawing, 10)
-
-    # process Proteus data recursively
-    process_node(plant_model, drawing, model_context)
-
-    # final statement
-    drawing.save()
+        cherrypy.engine.start()
+        cherrypy.engine.block()
