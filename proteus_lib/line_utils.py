ee87fb4ba88ea3c5d2db0952e8cd9a2bb7b95cb9
Timur Samkharadze <timur.samkharadze@akersolutions.com>
2020-02-11 14:55:27 +0100

    names for line types

--- proteus_lib/line_utils.py
+++ proteus_lib/line_utils.py
@@ -2,7 +2,7 @@ from proteus_lib.proteus_utils import ensure_type
 import xml.etree.ElementTree as XMLParse
 
 
-def fetch_line_type_from_presentation(pr_obj: XMLParse.Element, fallback = 'Solid') -> list:
+def fetch_line_type_from_presentation(pr_obj: XMLParse.Element, fallback='Solid') -> list:
     """
     function to fetch and convert Proteus line type to SVG stroke-dasharray
     One of the numbers or names from the following (Object Model document v2.2) :-
@@ -19,20 +19,20 @@ def fetch_line_type_from_presentation(pr_obj: XMLParse.Element, fallback = 'Soli
     """
     ensure_type(pr_obj, 'Presentation')
     line_type = pr_obj.attrib.get('LineType', fallback).lower()
-    if '0' or 'solid' == line_type:
+    if '0' == line_type or 'solid' == line_type:
         return []
-    if '1' or 'dotted' == line_type:
+    if '1' == line_type or 'dotted' == line_type:
         return [1]
-    if '2' or 'dashed' == line_type:
+    if '2' == line_type or 'dashed' == line_type:
         return [3]
-    if '3' or 'long dash' == line_type:
+    if '3' == line_type or 'long dash' == line_type:
         return [4, 1]
-    if '4' or 'long dash + short dash, centerline' == line_type:
+    if '4' == line_type or 'long dash + short dash, centerline' == line_type:
         return [4, 1, 2, 1]
-    if '5' or 'short dash' == line_type:
+    if '5' == line_type or 'short dash' == line_type:
         return [2, 1]
-    if '6' or 'long dash + short dash + short dash' == line_type:
+    if '6' == line_type or 'long dash + short dash + short dash' == line_type:
         return [4, 1, 2, 1, 2, 1]
-    if '7' or 'dash + short dash' == line_type:
+    if '7' == line_type or 'dash + short dash' == line_type:
         return [3, 1, 2, 1]
     raise ValueError(f'unknown line type ({line_type}) found')
