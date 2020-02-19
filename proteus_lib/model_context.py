c738dd402914b84e50c9b02658b545a28155d82f
Timur Samkharadze <timur.samkharadze@akersolutions.com>
2020-02-19 13:48:31 +0100

    added shape catalogue and function to get data from it

--- proteus_lib/model_context.py
+++ proteus_lib/model_context.py
@@ -1,5 +1,6 @@
 from dataclasses import dataclass
 from enum import Enum
+import lxml.etree as xml
 
 import svgwrite
 
@@ -20,3 +21,15 @@ class Context:
     origin: str = None
     debug: bool = False
     units: Unit = Unit.mm
+    shape_catalog: xml._Element = None
+
+    def get_from_shape_catalog(self, node_type, component_name):
+        """
+        Method to find if a symbol fo given type and component name exists in the shape catalogue
+        :param node_type:
+        :param component_name:
+        :return: xml._Element if found or None otherwise
+        """
+        if self.shape_catalog is not None:
+            return self.shape_catalog.find(f'.//{node_type}[@ComponentName="{component_name}"]')
+        return None
